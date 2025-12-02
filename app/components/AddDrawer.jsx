2acfc363642ed8127a10d37629d21a47df7073e0
vamshi thatikonda <thatikondavamshi13@gmail.com>
2025-12-02 17:36:10 +0530

    Almost final layout for adding amount

--- app/components/AddDrawer.jsx
+++ app/components/AddDrawer.jsx
@@ -1,23 +1,17 @@
 "use client";
 
 import * as React from "react";
-import { Grid, Plus } from "lucide-react";
+import { Plus } from "lucide-react";
+import { CalendarDays } from "lucide-react";
 
+import { Check } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer";
-import { Label } from "@/components/ui/label";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { ArrowLeft } from "lucide-react";
 
 export function AddDrawer() {
+  const [expenseAmount, setExpenseAmount] = React.useState("0");
+
   return (
     <Drawer>
       <DrawerTrigger asChild className="fixed bottom-8 right-8">
@@ -34,42 +28,62 @@ export function AddDrawer() {
       </DrawerTrigger>
       <DrawerContent>
         <div className="mx-auto h-lvh z-150 w-full max-w-md flex-col space-y-6 overflow-y-auto p-6">
-          <div className="flex h-full flex-col justify-between ">
+          <div className="flex h-full flex-col justify-between gap-1">
             {/* Amount Display */}
-            <div className="w-full p-3 rounded-sm hover:bg-secondary flex items-center space-x-4">
-              <input
-                type="text"
+            <div className="w-full p-1 rounded-sm hover:bg-secondary/60 flex items-center space-x-4">
+              <div
                 className={
-                  " outline-none rounded-none border-none w-full shadow-none ml-auto mainHeading4xl text-right focus:ring-0 focus:ring-offset-0 text-5xl sm:text-6xl"
+                  "outline-none p-2 rounded-none border-none w-full shadow-none ml-auto mainHeading4xl text-left focus:ring-0 focus:ring-offset-0 text-5xl sm:text-6xl"
                 }
-                reverse={true}
-                autoFocus
-                placeholder="0"
-              />
-              <div className="p-2 rounded-full cursor-pointer">
-                <ArrowLeft className="size-6 text-muted-foreground" />
+              >
+                ₹ <span className="text-foreground">{expenseAmount}</span>
+              </div>
+              <div className="p-2 flex items-center z-10 active:bg-secondary transition-all duration-150 justify-center rounded-sm h-full aspect-square cursor-pointer hover:bg-secondary">
+                <ArrowLeft
+                  className="size-6 text-muted-foreground"
+                  onClick={() => {
+                    setExpenseAmount((prev) => {
+                      return prev.length === 1 ? "0" : prev.slice(0, -1);
+                    });
+                  }}
+                />
               </div>
             </div>
             {/* Amount Numbers Gird */}
             <div>
               <div className="w-full grid grid-cols-4 gap-4">
                 <div className="grid col-span-3 grid-cols-3 gap-4 place-items-center">
-                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, ".", 0, "Save"].map((item) => (
+                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, ".", 0].map((item) => (
                     <Button
                       key={item}
-                      variant="outline"
-                      className="h-16 w-16 rounded-md text-2xl active:scale-95 active:bg-secondary transition-all duration-300"
+                      className="h-16 w-16 rounded-full font-semibold text-xl active:scale-95 active:bg-secondary transition-all duration-300"
+                      onClick={() => {
+                        setExpenseAmount((prev) => {
+                          return prev === "0"
+                            ? item.toString()
+                            : prev + item.toString();
+                        });
+                      }}
                     >
                       {item}
                     </Button>
                   ))}
+                  <Button
+                    variant="outline"
+                    className="h-16 w-16 rounded-md subLabel2 flex flex-col justify-center items-center space-y-1 active:scale-95 active:bg-secondary transition-all duration-300"
+                  >
+                    <Check size={24} strokeWidth={2} />
+                    <div>Save</div>
+                  </Button>
                 </div>
+
                 <div className="grid gird-cols-1 grid-rows-4 place-self-center gap-4">
                   <Button
                     variant="outline"
-                    className="h-16 w-16 rounded-md text-2xl"
+                    className="h-16 w-16 rounded-md subLabel2 flex flex-col justify-center items-center space-y-1 active:scale-95 active:bg-secondary transition-all duration-300"
                   >
-                    Date
+                    <CalendarDays size={24} strokeWidth={2} />
+                    <div>Today</div>
                   </Button>
                 </div>
               </div>
