ddd3e0b6f996898009fab045b24fe66ef4fe0f20
vamshi thatikonda <thatikondavamshi13@gmail.com>
2025-12-11 17:14:57 +0530

    date start

--- app/components/DateSelect.jsx
+++ app/components/DateSelect.jsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { DrawerContent, DrawerTrigger, Drawer } from "@/components/ui/drawer";
 import {
   Select,
   SelectContent,
@@ -21,19 +23,58 @@ const DateSelect = () => {
     { value: 10, name: "Nov" },
     { value: 11, name: "Dec" },
   ];
+  const years = () => {
+    const current = new Date().getFullYear();
+    const list = [];
+
+    for (let i = 0; i < 20; i++) {
+      list.push(current - i);
+    }
+
+    return list;
+  };
+
+  console.log("YEARS", years());
+
   return (
-    <div>
-      <Select onValueChange={(value) => console.log(value)}>
-        <SelectTrigger className="border-b-2 border-blue-500">
-          <SelectValue placeholder="June" />
-        </SelectTrigger>
-        <SelectContent className={"text-base"}>
-          {months.map((month) => (
-            <SelectItem value={month.value}>{month.name}</SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-    </div>
+    <Drawer
+      className="p-0"
+      onOpenChange={(open) => {
+        if (open) {
+        } else {
+        }
+      }}
+    >
+      <DrawerTrigger asChild className="border-b-2 border-blue-500">
+        <div className="flex justify-center items-center">June</div>
+      </DrawerTrigger>
+      <DrawerContent className={"p-1"}>
+        <div className="flex justify-around items-center p-3">
+          <div
+            className={
+              "text-base h-60 overflow-y-auto overflow-hidden [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden"
+            }
+          >
+            {months.map((month) => (
+              <div className="h-20 flex items-center justify-center">
+                <div>{month.name}</div>
+              </div>
+            ))}
+          </div>
+          <div
+            className={
+              "text-base h-60 overflow-y-auto overflow-hidden [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden"
+            }
+          >
+            {years().map((year) => (
+              <div className="h-20 flex items-center justify-center">
+                <div>{year}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </DrawerContent>
+    </Drawer>
   );
 };
 
